5a9e8705a76583518a0c11ce18cad057a997ccc6
unknown <guidowe45@gmail.com>
2022-04-27 12:43:09 -0300

    actualizo rar clase 1

---
+++ Clase 1 - R base/Clase_1_Introduccion.R
@@ -0,0 +1,206 @@
+A <- 1
+A 
+
+A + 6
+
+B <- 2
+B
+
+A <- 150
+B <- 132
+
+A = B #Ahora A toma el valor de B 
+print(A)
+
+#(B continua conservando el mismo valor)
+
+#suma 
+5+6
+
+#Resta
+6-8
+
+#cociente
+6/2
+
+#multiplicacion
+6*2
+
+#Potencia
+3^3
+
+
+#Redefinimos los valores A y B
+A <- 10
+B <- 20
+
+#Realizamos comparaciones lógicas
+A > B
+A >= B
+A < B
+A <= B
+A == B
+A != B
+
+
+C <- A != B
+C
+
+#Redefinimos los valores A y B
+A <- 50
+B <- 72
+#Realizamos comparaciones lógicas
+
+(A > 60 & B > 65)
+(A < 60 | B < 65) 
+
+
+## Practica:     
+#1) Crear un objeto llamado "mi_numero" que almecene un número que ustedes quieran. ¿Qué pasa si lo intentan nombrar como "mi numero"?         
+#2) Realizar alguna operación matemática con el objeto y guardar el resultado como un nuevo objeto llamado "otro_numero".     
+#3) Comprobar lógicamente si el segundo objeto es igual a 10.    
+#4) Comprobar lógicamente si alguno de los dos objetos es mayor a 10 
+
+
+A <- 1
+class(A)
+
+A <- "Hola, ¿qué tal?"
+
+class(A)
+
+class(Sys.Date())
+
+# Vector numérico
+vec_num <- c(1, 2, 2,2,3,2,1, 2, 1, 1, 1)
+class(vec_num)
+
+# Vector de caracteres
+dias_char <- c("Lunes","Viernes","Viernes","Jueves",
+       "Martes", "Martes","Miercoles","Martes",
+       "Miercoles")
+class(dias_char)
+
+# Vector de tipo fecha
+vec_fecha <- Sys.Date()+2
+
+class(vec_fecha)
+
+
+table(dias_char)
+
+# Una propiedad del factor: las etiquetas.
+dias_factor <- factor(dias_char,
+                 levels = c("Lunes",
+                            "Martes",
+                            "Miercoles",
+                            "Jueves",
+                            "Viernes"))
+class(dias_factor)
+
+table(dias_factor)
+
+as.numeric(dias_factor)
+
+D <- c(1, 3, 4)
+D <- D + 2
+D
+
+E <- D + 1:3 #esto es equivalente a hacer 3+1, 5+2, 6+9 
+E
+
+# Si quiero al valor 2 del objeto E:
+E 
+E[2]
+
+E_posicion2 <-  E[2]
+E_posicion2
+
+rm(E_posicion2)
+E_posicion2
+
+E
+E[2] <- "Pablo"
+E
+
+# Tener cuidado al modificar el tipo de uno de los valores y no el de todos los del objeto:
+class(E)
+
+## Practica:     
+# 1) Crear un vector numerico de 7 valores, elegir numeros entre 1 y 50        
+# 2) Comprobar logicamente cuales de ellos son menores a 25    
+# 3) Extraer del vector el 3er y 5to valor      
+# 4) Extraer del vector todos los valores menores a 25     
+
+AGLOMERADO  <- c(32,33,33,33,32)
+
+SEXO  <-  c("Varon","Mujer","Mujer","Varon","Mujer")
+
+EDAD  <-  c(60,54,18,27,32)
+
+Datos <- data.frame(AGLOMERADO, SEXO, EDAD)
+Datos
+class(Datos)
+
+Datos[3,2]
+Datos[4,3]
+
+Datos$AGLOMERADO
+class(Datos$AGLOMERADO)
+
+Datos$AGLOMERADO[2]
+
+Datos$AGLOMERADO[3,2]
+
+Datos[Datos$AGLOMERADO==32, ]
+
+Edad_Aglo32 <- Datos[Datos$AGLOMERADO==32,"EDAD"]
+Edad_Aglo32
+
+## Otra forma de llegar a lo mismo
+Datos$EDAD[Datos$AGLOMERADO==32]
+
+mean(Edad_Aglo32)
+
+LISTA <- list(A,B,C,D,E,Datos$AGLOMERADO, DF = Datos)
+LISTA
+
+LISTA$DF
+LISTA$DF$EDAD
+LISTA$DF$EDAD[2]
+
+LISTA[[6]]
+
+LISTA[[6]][1]
+
+LISTA[[7]][2]
+
+LISTA[[7]][2,1]
+
+paste("Pega","estas", 4, "palabras", sep = " ")
+
+#Puedo concatenar caracteres almacenados en objetos
+a <- c(1, 2, 3)
+b <- "con"
+c <- c(4, 5, 6)
+
+paste(a,b,c,sep = "-")
+
+# Paste0 pega los caracteres sin separador
+paste0(a,b,c)
+
+# Repasando un comando ya visto...
+1:5
+
+# Función para sumar
+sum(1:5)
+
+# Para calclar medias
+mean(1:5)
+
+#Maximo
+max(Datos$EDAD)
+
+#Valores únicos de un vector
+unique(Datos$AGLOMERADO)
+
